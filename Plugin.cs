e9c13715879cb66adb53330e13830139ee66f8e4
Zayne <142189531+zaynethedev@users.noreply.github.com>
2024-12-06 22:34:08 -0800

    clean up Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -15,15 +15,14 @@ namespace Index
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     public class Plugin : BaseUnityPlugin
     {
-        public static bool inRoom, initialized;
+        public static bool inRoom = false, initialized = false;
         public static List<IndexMod> mods = new List<IndexMod>();
         public static GameObject indexPanel;
 
         void Start()
         {
-            var bundle = LoadAssetBundle("Index.Resources.index");
-            indexPanel = bundle.LoadAsset<GameObject>("IndexPanel");
-            GorillaTagger.OnPlayerSpawned(OnGameInitialized);
+            preInit();
+            GorillaTagger.OnPlayerSpawned(init);
         }
 
         void OnEnable()
@@ -36,68 +35,93 @@ namespace Index
             HarmonyPatches.RemoveHarmonyPatches();
         }
 
-        void OnGameInitialized()
+        void preInit()
+        {
+            var bundle = LoadAssetBundle("Index.Resources.index");
+            indexPanel = bundle.LoadAsset<GameObject>("IndexPanel");
+        }
+
+        void init()
         {
             var allTypes = Assembly.GetExecutingAssembly().GetTypes();
-            indexPanel = indexPanel.transform.Find("Pivot").gameObject;
-            indexPanel = Instantiate(indexPanel);
-            indexPanel.AddComponent<DevHoldable>();
-            var indexTransform = indexPanel.transform;
-            indexTransform.localPosition = new Vector3(-67.3437f, 12f, -81.9055f);
-            indexTransform.localScale = new Vector3(0.16f, 0.16f, 0.16f);
-            indexTransform.rotation = Quaternion.Euler(0f, 335f, 0f);
-            var indexPanelMods = indexTransform.Find("Mods");
-            indexTransform.Find("Page1").gameObject.AddComponent<ButtonManager>();
-            indexTransform.Find("Page2").gameObject.AddComponent<ButtonManager>();
-            indexPanel.SetActive(false);
-            indexTransform.Find("Mods/page2").gameObject.SetActive(false);
-            var modParent = new GameObject("Index.Mods");
 
+            indexPanel = Instantiate(indexPanel.transform.Find("Pivot").gameObject);
+            indexPanel.AddComponent<DevHoldable>();
+            InitializePanelTransform();
+            SetupModPanel();
             foreach (var type in allTypes)
             {
                 if (typeof(IndexMod).IsAssignableFrom(type) && !type.IsAbstract)
                 {
-                    IndexMod modInstance = (IndexMod)Activator.CreateInstance(type);
-                    mods.Add(modInstance);
-
-                    GameObject modGameObject = new GameObject(modInstance.modName);
-                    modGameObject.AddComponent(modInstance.GetType());
-                    modGameObject.transform.parent = modParent.transform;
-                    modInstance.Start();
-                    Transform panelMod = indexPanelMods.Find($"{modInstance.modID}");
-
-                    TextMeshPro textComponent = panelMod.Find("Text")?.GetComponent<TextMeshPro>();
-                    if (textComponent != null)
-                    {
-                        textComponent.text = modInstance.modName.ToString();
-                    }
-                    if (!panelMod.GetComponent<ButtonManager>())
-                    {
-                        var buttonManager = panelMod.gameObject.AddComponent<ButtonManager>();
-                        buttonManager.Start();
-                    }
-
-                    Transform page1 = indexPanelMods.Find("page1");
-                    Transform page2 = indexPanelMods.Find("page2");
-                    if (new HashSet<string> { "1", "2", "3", "4", "5", "6", "7", "8" }.Contains(panelMod.gameObject.name))
-                    {
-                        if (page1 != null)
-                        {
-                            panelMod.transform.SetParent(page1, false);
-                        }
-                    }
-                    else if (new HashSet<string> { "9", "10", "11", "12", "13", "14" }.Contains(panelMod.gameObject.name))
-                    {
-                        if (page2 != null)
-                        {
-                            panelMod.transform.SetParent(page2, false);
-                        }
-                    }
-
-                    Debug.Log($"INDEX // {modInstance.modName} initialized correctly.");
-                    initialized = true;
+                    InitializeMod(type);
                 }
             }
+            DisableUnusedMods();
+            initialized = true;
+            Debug.Log("INDEX Initialization complete.");
+        }
+
+        void InitializePanelTransform()
+        {
+            var indexTransform = indexPanel.transform;
+            indexTransform.localPosition = new Vector3(-67.3437f, 12f, -81.9055f);
+            indexTransform.localScale = new Vector3(0.16f, 0.16f, 0.16f);
+            indexTransform.rotation = Quaternion.Euler(0f, 335f, 0f);
+        }
+
+        void SetupModPanel()
+        {
+            var indexTransform = indexPanel.transform;
+            var indexPanelMods = indexTransform.Find("Mods");
+            indexTransform.Find("Page1").gameObject.AddComponent<ButtonManager>();
+            indexTransform.Find("Page2").gameObject.AddComponent<ButtonManager>();
+            indexTransform.Find("Mods/page2").gameObject.SetActive(false);
+            indexPanel.SetActive(false);
+        }
+
+        void InitializeMod(Type modType)
+        {
+            IndexMod modInstance = (IndexMod)Activator.CreateInstance(modType);
+            mods.Add(modInstance);
+            GameObject modGameObject = new GameObject(modInstance.modName);
+            modGameObject.AddComponent(modType);
+            modGameObject.transform.parent = indexPanel.transform.Find("Mods").transform;
+            modInstance.Start();
+            SetupModUI(modInstance);
+            Debug.Log($"INDEX // {modInstance.modName} initialized correctly.");
+        }
+
+        void SetupModUI(IndexMod modInstance)
+        {
+            var modPanel = indexPanel.transform.Find($"Mods/{modInstance.modID}");
+            TextMeshPro textComponent = modPanel.Find("Text")?.GetComponent<TextMeshPro>();
+            if (textComponent != null)
+            {
+                textComponent.text = modInstance.modName;
+            }
+            if (!modPanel.GetComponent<ButtonManager>())
+            {
+                var buttonManager = modPanel.gameObject.AddComponent<ButtonManager>();
+                buttonManager.Start();
+            }
+            DistributeModToPage(modPanel);
+        }
+
+        void DistributeModToPage(Transform modPanel)
+        {
+            var modName = modPanel.gameObject.name;
+            var page1 = indexPanel.transform.Find("Mods/page1");
+            var page2 = indexPanel.transform.Find("Mods/page2");
+            if (new HashSet<string> { "1", "2", "3", "4", "5", "6", "7", "8" }.Contains(modName))
+                modPanel.SetParent(page1, false);
+            else if (new HashSet<string> { "9", "10", "11", "12", "13", "14" }.Contains(modName))
+                modPanel.SetParent(page2, false);
+        }
+
+        void DisableUnusedMods()
+        {
+            var indexPanelMods = indexPanel.transform.Find("Mods");
+
             foreach (Transform child in indexPanelMods)
             {
                 if (child.name != "page1" && child.name != "page2")
@@ -106,85 +130,84 @@ namespace Index
                     child.gameObject.SetActive(false);
                 }
             }
-
-            Debug.Log("INDEX Initialization complete.");
         }
 
         void Update()
         {
-            if (initialized)
+            if (!initialized) return;
+
+            /*if (Keyboard.current.jKey.wasPressedThisFrame)
+                PhotonNetworkController.Instance.AttemptToJoinSpecificRoom("F6", JoinType.Solo);*/
+
+            if (PhotonNetwork.InRoom && PhotonNetwork.CurrentRoom.CustomProperties["gameMode"].ToString().Contains("MODDED"))
+                foreach (IndexMod index in mods)
+                    if (index.enabled)
+                        index.OnUpdate();
+        }
+
+        void FixedUpdate()
+        {
+            if (!initialized) return;
+
+            if (PhotonNetwork.InRoom && PhotonNetwork.CurrentRoom.CustomProperties["gameMode"].ToString().Contains("MODDED"))
             {
-                if (mods != null)
+                HandleModPanelVisibility();
+                HandleModUpdates();
+            }
+            else
+            {
+                if (inRoom)
                 {
-                    if (PhotonNetwork.InRoom && PhotonNetwork.CurrentRoom.CustomProperties["gameMode"].ToString().Contains("MODDED"))
+                    inRoom = false;
+                }
+                if (indexPanel.activeSelf)
+                {
+                    indexPanel.SetActive(false);
+                }
+                foreach (IndexMod index in mods)
+                {
+                    if (index.enabled)
                     {
-                        foreach (IndexMod index in mods)
-                        {
-                            if (index.enabled)
-                            {
-                                index.OnUpdate();
-                            }
-                        }
+                        index.OnModDisabled();
                     }
                 }
             }
         }
-        void FixedUpdate()
+
+        void HandleModPanelVisibility()
         {
-            if (initialized)
+            if (!inRoom) inRoom = true;
+
+            if (ControllerInputPoller.instance.leftControllerPrimaryButton && ControllerInputPoller.instance.rightControllerPrimaryButton)
             {
-                if (mods != null)
+                var offset = new Vector3(0.4f, 0, 0.4f);
+                indexPanel.transform.position = Player.Instance.headCollider.transform.position + offset;
+                indexPanel.transform.rotation = Player.Instance.headCollider.transform.rotation;
+            }
+
+            if (!indexPanel.activeSelf)
+            {
+                indexPanel.SetActive(true);
+            }
+        }
+
+        void HandleModUpdates()
+        {
+            foreach (IndexMod index in mods)
+            {
+                if (index.enabled)
                 {
-                    if (PhotonNetwork.InRoom && PhotonNetwork.CurrentRoom.CustomProperties["gameMode"].ToString().Contains("MODDED"))
-                    {
-                        if (!inRoom)
-                            inRoom = true;
-
-                        if (ControllerInputPoller.instance.leftControllerPrimaryButton && ControllerInputPoller.instance.rightControllerPrimaryButton)
-                        {
-                            Vector3 offset = new Vector3(0.4f, 0, 0.4f);
-                            indexPanel.transform.position = Player.Instance.headCollider.transform.position + offset;
-                            indexPanel.transform.rotation = Player.Instance.headCollider.transform.rotation;
-                        }
-
-                        if (!indexPanel.activeSelf)
-                            indexPanel.SetActive(true);
-
-                        foreach (IndexMod index in mods)
-                        {
-                            if (index.enabled)
-                            {
-                                index.OnFixedUpdate();
-                            }
-                        }
-                    }
-                    else
-                    {
-                        if (inRoom)
-                            inRoom = false;
-
-                        if (indexPanel.activeSelf)
-                            indexPanel.SetActive(false);
-
-                        foreach (IndexMod index in mods)
-                        {
-                            if (index.enabled)
-                            {
-                                index.OnModDisabled();
-                            }
-                        }
-
-                    }
+                    index.OnFixedUpdate();
                 }
             }
         }
 
         public AssetBundle LoadAssetBundle(string path)
         {
-            Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path);
-            AssetBundle bundle = AssetBundle.LoadFromStream(stream);
-            stream.Close();
-            return bundle;
+            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path))
+            {
+                return AssetBundle.LoadFromStream(stream);
+            }
         }
     }
 }
