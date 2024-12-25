318b13e4c323fd2ce3c500594d364707b25e0ccc
Xenon <143745702+XenonOrSomething@users.noreply.github.com>
2024-12-25 18:39:13 +0000

    frozone

---
+++ Scripts/SlideScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Index.Scripts
+{
+    public class SlideScript : MonoBehaviour
+    {
+
+        public float secs = 1.0f;
+        void Start() => StartCoroutine(Break());
+
+        public IEnumerator Break()
+        {
+            yield return new WaitForSeconds(secs);
+            Destroy(gameObject);
+        }
+
+        
+    }
+}
