cbf08ceeb22dfd80a9e39ce86bac91b505bc286d
Zayne <142189531+zaynethedev@users.noreply.github.com>
2025-02-02 14:28:20 -0800

    Rework wall walking

--- Mods/WallWalk.cs
+++ Mods/WallWalk.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 
 namespace Index.Mods
 {
+<<<<<<< Updated upstream
     [IndexMod("Wall Walk", "Lets you smoothly walk on walls.", "WallWalk", 4)]
+=======
+    [IndexMod("Wall Walk", "Lets you smoothly walk on walls and ceilings.", "WallWalk", 4)]
+>>>>>>> Stashed changes
     class WallWalk : ModHandler
     {
         public static WallWalk instance;
@@ -13,7 +17,12 @@ namespace Index.Mods
         public Vector3 originalGravity;
         private Vector3 currentGravity;
         private Vector3 targetGravity;
+<<<<<<< Updated upstream
         private float smoothness = 5.0f;
+=======
+        private float lerp = 6.5f;
+        private float connect = 1.5f;
+>>>>>>> Stashed changes
 
         public override void Start()
         {
@@ -31,15 +40,28 @@ namespace Index.Mods
             if (Player.Instance.wasLeftHandTouching || Player.Instance.wasRightHandTouching)
             {
                 hit = GetLastHitInfo();
+<<<<<<< Updated upstream
                 Vector3 wallNormal = hit.normal;
                 Vector3 playerUp = Player.Instance.bodyCollider.transform.up;
                 targetGravity = Vector3.Lerp(playerUp * -originalGravity.magnitude, wallNormal * -originalGravity.magnitude, 0.8f);
+=======
+                if (hit.distance <= connect)
+                {
+                    Vector3 surfaceNormal = hit.normal;
+                    Vector3 playerUp = Player.Instance.bodyCollider.transform.up;
+                    targetGravity = Vector3.Lerp(playerUp * -originalGravity.magnitude, surfaceNormal * -originalGravity.magnitude, 3f);
+                }
+>>>>>>> Stashed changes
             }
             else
             {
                 targetGravity = originalGravity;
             }
+<<<<<<< Updated upstream
             currentGravity = Vector3.Lerp(currentGravity, targetGravity, Time.deltaTime * smoothness);
+=======
+            currentGravity = Vector3.Lerp(currentGravity, targetGravity, Time.deltaTime * lerp);
+>>>>>>> Stashed changes
             Physics.gravity = currentGravity;
         }
 
