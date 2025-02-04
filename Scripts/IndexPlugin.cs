823d1fcd9451048220b0cfbabdb309f31f793723
Zayne <142189531+zaynethedev@users.noreply.github.com>
2025-02-03 22:06:53 -0800

    Setup
    PluginHandler/IndexPlugin setup with some base functions/variables.

---
+++ Scripts/IndexPlugin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Index.Scripts
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class IndexPlugin : Attribute
+    {
+        public string name { get; }
+        public string description { get; }
+        public string guid { get; }
+
+        public IndexPlugin(string Name, string Description, string GUID)
+        {
+            name = Name;
+            description = Description;
+            guid = GUID;
+        }
+    }
+}
---
+++ Scripts/PluginHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Index.Scripts
+{
+    internal class PluginHandler : MonoBehaviour
+    {
+        public List<string> LP_GUIDLIST = new List<string>();
+        public List<IndexPlugin> LP_LIST = new List<IndexPlugin>();
+
+        void Start()
+        {
+
+        }
+
+        private void CheckPlugins()
+        {
+
+        }
+
+        private void LoadPlugin()
+        {
+
+        }
+    }
+}
