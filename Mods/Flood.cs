6a8529e83b11dd295c58790cfce7ba43f352e7b8
Zayne <142189531+zaynethedev@users.noreply.github.com>
2025-03-24 19:11:25 -0700

    Add new mods (Flood & SlideControl)

---
+++ Mods/Flood.cs
@@ -0,0 +1,47 @@
+using Index.Resources;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Index.Mods
+{
+    [IndexMod("Flood", "Use your triggers to control how high the water rises! Left Trigger: Lower, Right Trigger: Rise", "Flood", 17)]
+    class Flood : ModHandler
+    {
+        public static Flood instance;
+        public GameObject floodWater;
+
+        public override void Start()
+        {
+            base.Start();
+            instance = this;
+        }
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+            if (ControllerInputPoller.instance.leftControllerIndexFloat >= 0.5f)
+            {
+                floodWater.transform.position = new Vector3(floodWater.transform.position.x, floodWater.transform.position.y - 0.075f, floodWater.transform.position.z);
+            }
+            if (ControllerInputPoller.instance.rightControllerIndexFloat >= 0.5f)
+            {
+                floodWater.transform.position = new Vector3(floodWater.transform.position.x, floodWater.transform.position.y + 0.075f, floodWater.transform.position.z);
+            }
+        }
+
+        public override void OnModDisabled()
+        {
+            base.OnModDisabled();
+            GameObject.Destroy(floodWater);
+        }
+
+        public override void OnModEnabled()
+        {
+            base.OnModEnabled();
+            floodWater = GameObject.Instantiate(GameObject.Find("CaveWaterVolume"));
+            floodWater.transform.localScale = new Vector3(3f, 500, 5f);
+            floodWater.transform.position = new Vector3(-50, -5f, -60f);
+            floodWater.transform.rotation = Quaternion.identity;
+        }
+    }
+}
---
+++ Mods/SlideControl.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx.Configuration;
+using GorillaLocomotion;
+using Index.Resources;
+using UnityEngine;
+
+namespace Index.Mods
+{
+    [IndexMod("Slide Control", "Increases your slide control on slippery surfaces.", "SlideControl", 18)]
+    class SlideControl : ModHandler
+    {
+        public static SlideControl instance;
+        public ConfigEntry<float> slideControlMultiplier;
+        private float slideControl;
+
+        public override void Start()
+        {
+            base.Start();
+            instance = this;
+        }
+
+        public override void SetConfig()
+        {
+            base.SetConfig();
+            slideControlMultiplier = Plugin.config.Bind(
+                section: "Slide Control",
+                key: "Slide Control Multiplier",
+                defaultValue: 3.25f,
+                description: "Changes your slide control. 2.5 = weak, 5 = extreme."
+            );
+        }
+
+        public override void OnModDisabled()
+        {
+            base.OnModDisabled();
+            GTPlayer.Instance.slideControl = slideControl;
+        }
+
+        public override void OnModEnabled()
+        {
+            base.OnModEnabled();
+            slideControl = GTPlayer.Instance.slideControl;
+            GTPlayer.Instance.slideControl = slideControl * Mathf.Clamp(slideControlMultiplier.Value, 2.5f, 5f);
+        }
+    }
+}
